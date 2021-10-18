0af08962bbddbe6da61505ceee05fe0168109e7a
TheLordOfAllMuffins <nopika99@gmail.com>
2021-10-18 00:51:36 -0700

    Basic 1-d case implementation

---
+++ band_calc.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+import matplotlib.pyplot as p
+
+from math import pi
+
+# a free electron's energy is equal to hb^2/(2m) * k^2, but I'm starting with just k^2 for simplicity
+def energy(k: float) -> float:
+    return k**2
+
+# this'll be how many "G"s out to plot; won't work well for 2d or 3d but we'll cross those bridges when we get to them
+n: int = 1
+
+# our direct lattice spacing (starting easy, as normal)
+a: float = 1
+
+# reciprocal lattice spacing
+b: float = 2*pi/a
+
+
+
+for m in range(-n, n+1):
+    ks = np.linspace(-b/2, b/2, num = 50)
+    energies = []
+    for k in ks:
+        energies.append(energy(k + m*b))
+    p.plot(ks, energies)
+
+p.xlabel("k")
+p.ylabel("Not Energy Exactly")
+p.show()
