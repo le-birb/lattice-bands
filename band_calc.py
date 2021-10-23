679d9db77c64298634c4e8e0794404f3134ebda0
TheLordOFALlMuffins <nopika99@gmail.com>
2021-10-22 21:01:12 -0700

    Change axis labels to better match conventions

--- band_calc.py
+++ band_calc.py
@@ -9,7 +9,7 @@ def energy(k: float) -> float:
     return k**2
 
 # this'll be how many "G"s out to plot; won't work well for 2d or 3d but we'll cross those bridges when we get to them
-n: int = 1
+n: int = 2
 
 # our direct lattice spacing (starting easy, as normal)
 a: float = 1
@@ -17,6 +17,9 @@ a: float = 1
 # reciprocal lattice spacing
 b: float = 2*pi/a
 
+
+fig = p.figure()
+ax  = fig.add_subplot()
 
 # m controls the G we're looking at, with G = mb
 # we take values from -n to n inclusive (n+1 is excluded by range())
@@ -31,8 +34,10 @@ for m in range(-n, n+1):
         # the energies are taken from the "true" position k + G
         energies.append(energy(k + m*b))
 
-    p.plot(ks, energies)
+    ax.plot(ks, energies)
 
-p.xlabel("k")
-p.ylabel("Not Energy Exactly")
+ax.set_xlabel("k")
+ax.set_xticks([-np.pi, 0, np.pi])
+ax.set_xticklabels([r"$-\frac{π}{a}$", "0", r"$\frac{π}{a}$"])
+ax.set_ylabel(r"Energy, in units of $\frac{ħ^2}{2m}(\frac{π}{a})^2$")
 p.show()
