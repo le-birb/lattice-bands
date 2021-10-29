2e29b55cdfa01f62d07e53fc65c3454230ee3608
TheLordOFALlMuffins <nopika99@gmail.com>
2021-10-28 18:13:13 -0700

    Add structural code for degenerate band handling

--- 2d_band_calc.py
+++ 2d_band_calc.py
@@ -52,6 +52,11 @@ g_y = range(-g_range, g_range + 1)
 
 g_offsets = product(g_x, g_y)
 
+
+seen_gamma_x_endpoints: set = {}
+seen_x_m_endpoints: set = {}
+seen_m_gamma_endpoints: set = {}
+
 # TODO: keep track of degenerate bands
 # current idea: hold a set of start and end points for each band
 # that's been plotted, and compare each new start/end point
@@ -63,6 +68,22 @@ for offset in g_offsets:
     gamma_x_energies = energy(gamma_x_kx + offset[0]*b[0], gamma_x_ky + offset[1]*b[1])
     x_m_energies = energy(x_m_kx + offset[0]*b[0], x_m_ky + offset[1]*b[1])
     m_gamma_energies = energy(m_gamma_kx + offset[0]*b[0], m_gamma_ky + offset[1]*b[1])
+
+    # check endpoints for degeneracy
+    gamma_x_endpoints = (gamma_x_energies[0], gamma_x_energies[-1])
+    x_m_endpoints = (x_m_energies[0], x_m_energies[-1])
+    m_gamma_endpoints = (m_gamma_energies[0], m_gamma_energies[-1])
+
+    if gamma_x_endpoints in seen_gamma_x_endpoints:
+        # degeneracy detected
+        # deus vult
+        pass
+
+    if x_m_endpoints in seen_x_m_endpoints:
+        pass
+
+    if m_gamma_endpoints in seen_m_gamma_endpoints:
+        pass
 
     ax.plot(gamma_x_plot_range, gamma_x_energies)
     ax.plot(x_m_plot_range, x_m_energies)
