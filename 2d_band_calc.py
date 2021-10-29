c3d761f0b1795fdcbf59fcb66cc080633d285c83
TheLordOFALlMuffins <nopika99@gmail.com>
2021-10-28 17:41:36 -0700

    Change pi to gamma the point's actually called gamma, not pi

--- 2d_band_calc.py
+++ 2d_band_calc.py
@@ -24,19 +24,19 @@ resolution = 50
 # each path is split into a separate list of x and y coordinates
 # so that they can be passed as arrays into the energy() function
 # with no extra fuss
-pi_x_kx = np.linspace(0, b[0]/2, num = resolution)
-pi_x_ky = np.zeros(resolution)
+gamma_x_kx = np.linspace(0, b[0]/2, num = resolution)
+gamma_x_ky = np.zeros(resolution)
 
 x_m_kx  = np.zeros(resolution) + b[0]/2
 x_m_ky  = np.linspace(0, b[1]/2, num = resolution)
 
-m_pi_kx = np.linspace(b[0]/2, 0, num = resolution)
-m_pi_ky = np.linspace(b[1]/2, 0, num = resolution)
+m_gamma_kx = np.linspace(b[0]/2, 0, num = resolution)
+m_gamma_ky = np.linspace(b[1]/2, 0, num = resolution)
 
 
-pi_x_plot_range = np.linspace(0, 2)
+gamma_x_plot_range = np.linspace(0, 2)
 x_m_plot_range  = np.linspace(2, 4)
-m_pi_plot_range = np.linspace(4, 6)
+m_gamma_plot_range = np.linspace(4, 6)
 
 
 fig = p.figure()
@@ -60,16 +60,16 @@ g_offsets = product(g_x, g_y)
 #  - might run into trouble around different path sections having
 #    the same start/end point
 for offset in g_offsets:
-    pi_x_energies = energy(pi_x_kx + offset[0]*b[0], pi_x_ky + offset[1]*b[1])
+    gamma_x_energies = energy(gamma_x_kx + offset[0]*b[0], gamma_x_ky + offset[1]*b[1])
     x_m_energies = energy(x_m_kx + offset[0]*b[0], x_m_ky + offset[1]*b[1])
-    m_pi_energies = energy(m_pi_kx + offset[0]*b[0], m_pi_ky + offset[1]*b[1])
+    m_gamma_energies = energy(m_gamma_kx + offset[0]*b[0], m_gamma_ky + offset[1]*b[1])
 
-    ax.plot(pi_x_plot_range, pi_x_energies)
+    ax.plot(gamma_x_plot_range, gamma_x_energies)
     ax.plot(x_m_plot_range, x_m_energies)
-    ax.plot(m_pi_plot_range, m_pi_energies)
+    ax.plot(m_gamma_plot_range, m_gamma_energies)
 
 ax.set_xlabel("High Symmetry Points")
 ax.set_xticks([0, 1, 2, 3, 4, 5, 6])
-ax.set_xticklabels([r"$\Pi$", r"$\Delta$", r"X", r"Z", r"M", r"$\Sigma$", r"$\Pi$"])
+ax.set_xticklabels([r"$\Gamma$", r"$\Delta$", r"X", r"Z", r"M", r"$\Sigma$", r"$\Gamma$"])
 ax.set_ylabel(r"Energy, in units of $\frac{ħ^2}{2m}(\frac{π}{a})^2$")
 p.show()
