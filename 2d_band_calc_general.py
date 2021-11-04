330d81fd5a46e4322c5c571dc1843ad56956a303
TheLordOfAllMuffins <nopika99@gmail.com>
2021-11-03 19:25:34 -0700

    Add general path handling

--- 2d_band_calc_general.py
+++ 2d_band_calc_general.py
@@ -1,4 +1,5 @@
 
+import itertools
 import numpy as np
 import matplotlib.pyplot as p
 
@@ -11,6 +12,12 @@ def energy(position) -> float:
 
 def triple_product(a, b, c) -> float:
     return np.dot(a, np.cross(b, c))
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 
 g_range = 1
@@ -36,4 +43,36 @@ np.array([
 ])
 
 b1, b2, b3 = reciprocal_basis
+
+# here's where the third dimension is ignored
+points = \
+[
+    (0,0,0),
+    (2*pi/(3*a), 0, 0),
+    (4*pi/(3*a), 0, 0),
+    (pi/a, pi/(sqrt(3)*a), 0),
+    (pi/(2*a), pi/(sqrt(3)*2*a)),
+    (0, 0, 0)
+]
+
+point_names = \
+[
+    "$\Gamma$",
+    "T",
+    "K",
+    "M",
+    "$\Sigma$",
+    "$\Gamma$"
+]
+
+resolution = 50
+
+path = np.array()
+
+for start, end in pairwise(points):
+    path.append(np.linspace(start, end, num = resolution, endpoint = False))
+
+path.append(np.array(end))
+
+plot_range = np.linspace(0, len(points)-1, num = resolution * (len(points)-1))
 
