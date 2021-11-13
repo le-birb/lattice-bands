58e6f70e81f72b251233f95822879522bfa0646e
TheLordOfAllMuffins <nopika99@gmail.com>
2021-11-12 16:29:46 -0800

    Modularize the general calc file Move functionality into functions so they can be called form elsewhere

--- 2d_band_calc_general.py
+++ 2d_band_calc_general.py
@@ -12,6 +12,25 @@ import tkinter
 from tkinter import filedialog
 import json
 
+
+class lattice:
+    def __init__(self, basis = [], points = [], point_names = [], vertical_lines = []):
+        self.basis = list(basis)
+        self.a1, self.a2, self.a3 = self.basis
+        self._direct_triple = triple_product(*self.basis)
+        
+        self.points = list(points)
+        self.point_names = list(point_names)
+        self.vertical_lines = list(vertical_lines)
+        
+        self.reciprocal_basis = np.array(
+        [
+            np.cross(self.a2, self.a3)/self._direct_triple,
+            np.cross(self.a3, self.a1)/self._direct_triple,
+            np.cross(self.a1, self.a2)/self._direct_triple
+        ])*(2*pi)
+        self.b1, self.b2, self.b3 = self.reciprocal_basis
+
 class degeneracy_tracker(defaultdict):
     __sentinel = object()
 
@@ -66,88 +85,79 @@ def pairwise(iterable):
     next(b, None)
     return zip(a, b)
 
-# this script doesn't use any part of tkinter than the file dialog,
-# so the other window that pops up is closed with withdraw()
-root = tkinter.Tk()
-root.withdraw()
-filename = filedialog.askopenfilename(initialdir = "lattices")
-with open(filename, "r") as f:
-    lattice_data: dict = json.load(f)
+def _get_lattice() -> lattice:
+    # this script doesn't use any part of tkinter than the file dialog,
+    # so the other window that pops up is closed with withdraw()
+    root = tkinter.Tk()
+    root.withdraw()
+    filename = filedialog.askopenfilename(initialdir = "lattices")
+    with open(filename, "r") as f:
+        lattice_data: dict = json.load(f)
+    return lattice(lattice_data["basis"], lattice_data["points"], lattice_data["point_names"], lattice_data["line_points"])
 
-direct_basis = np.array(lattice_data["basis"])
-a1, a2, a3 = direct_basis
-direct_triple = triple_product(*direct_basis)
+def plot_bands(lat: lattice, reciprocal_range = 1, resolution = 50):
+    reciprocal_range = 1
 
-reciprocal_basis = 2*pi* \
-np.array([
-    np.cross(a2, a3)/direct_triple,
-    np.cross(a3, a1)/direct_triple,
-    np.cross(a1, a2)/direct_triple
-])
-b1, b2, b3 = reciprocal_basis
+    resolution = 50
 
-points = lattice_data["points"]
-point_names = lattice_data["point_names"]
-line_points = lattice_data["line_points"]
+    paths = []
+    plot_ranges = []
+    curr_range_start = 0
+    seen_endpoints: list[degeneracy_tracker] = []
 
-reciprocal_range = 1
+    for start, end in pairwise(lat.points):
+        paths.append(np.linspace(start, end, num = resolution))
+        plot_ranges.append(np.linspace(curr_range_start, curr_range_start + 1, num = resolution))
+        curr_range_start += 1
+        seen_endpoints.append(degeneracy_tracker())
 
-resolution = 50
+    # TODO: rework this to visit only nth nearest-neighbors instead of
+    # all the points within n reciprocal basis vectors away in each direction
+    # this will help make sure that degeneracy is consistent
+    g_range = range(-reciprocal_range, reciprocal_range + 1)
+    # here is the second place to ignore the third dimension
+    g_offsets = set(product(g_range, g_range, (0,)))
+    g_offsets.remove((-1, 1, 0))
+    g_offsets.remove((1,-1,0))
 
-paths = []
-plot_ranges = []
-curr_range_start = 0
-seen_endpoints: list[degeneracy_tracker] = []
+    # TODO: make sure there's a handler for if this ever runs out
+    # if that situation is hit, come up with a better solution
 
-for start, end in pairwise(points):
-    paths.append(np.linspace(start, end, num = resolution))
-    plot_ranges.append(np.linspace(curr_range_start, curr_range_start + 1, num = resolution))
-    curr_range_start += 1
-    seen_endpoints.append(degeneracy_tracker())
+    # position 0 is None as it should never come up
+    degeneracy_colors = [None, "black", "red", "orange", "yellow", "green", "blue", "purple"]
 
-# TODO: rework this to visit only nth nearest-neighbors instead of
-# all the points within n reciprocal basis vectors away in each direction
-# this will help make sure that degeneracy is consistent
-g_range = range(-reciprocal_range, reciprocal_range + 1)
-# here is the second place to ignore the third dimension
-g_offsets = set(product(g_range, g_range, (0,)))
-g_offsets.remove((-1, 1, 0))
-g_offsets.remove((1,-1,0))
+    fig = p.figure()
+    ax  = fig.add_subplot()
 
-# TODO: make sure there's a handler for if this ever runs out
-# if that situation is hit, come up with a better solution
+    for point in lat.vertical_lines:
+        p.axvline(point, linestyle = "--", color = (0, 0, 0, .5))
 
-# position 0 is None as it should never come up
-degeneracy_colors = [None, "black", "red", "orange", "yellow", "green", "blue", "purple"]
+    for i in range(len(paths)):
+        path = paths[i]
+        plot_range = plot_ranges[i]
+        degeneracies = seen_endpoints[i]
 
-fig = p.figure()
-ax  = fig.add_subplot()
+        for offset in g_offsets:
+            actual_k = (offset*lat.reciprocal_basis).sum(0)
+            energies = energy(path + actual_k)
+            endpoints = (energies[0], energies[-1])
 
-for point in line_points:
-    p.axvline(point, linestyle = "--", color = (0, 0, 0, .5))
+            # degeneracy is checked for at the endpoints of bands, since there is only one
+            # possible path between 2 endpoints
+            degeneracies[endpoints] += 1
 
-for i in range(len(paths)):
-    path = paths[i]
-    plot_range = plot_ranges[i]
-    degeneracies = seen_endpoints[i]
+            ax.plot(plot_range, energies, color = degeneracy_colors[degeneracies[endpoints]])
 
-    for offset in g_offsets:
-        actual_k = (offset*reciprocal_basis).sum(0)
-        energies = energy(path + actual_k)
-        endpoints = (energies[0], energies[-1])
-
-        # degeneracy is checked for at the endpoints of bands, since there is only one
-        # possible path between 2 endpoints
-        degeneracies[endpoints] += 1
-
-        ax.plot(plot_range, energies, color = degeneracy_colors[degeneracies[endpoints]])
-
-ax.set_xlabel("High Symmetry Points")
-ax.set_xticks(list(range(len(points))))
-ax.set_xticklabels(point_names)
-ax.set_ylabel(r"Energy, in units of $\frac{ħ^2}{2m}(\frac{π}{a})^2$")
-p.show()
+    ax.set_xlabel("High Symmetry Points")
+    ax.set_xticks(list(range(len(lat.points))))
+    ax.set_xticklabels(lat.point_names)
+    ax.set_ylabel(r"Energy, in units of $\frac{ħ^2}{2m}(\frac{π}{a})^2$")
+    p.show()
 
 # TODO: figure out how to make the script end on its own
 # either that or wait until the problem isn't a problem anymore
 # when a fuller GUI is made
+
+if __name__ == "__main__":
+    lat = _get_lattice()
+    plot_bands(lat)
