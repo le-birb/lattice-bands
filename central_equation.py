3ea65809724ca4649f44f38e36da5bc9b018851c
TheLordOFALlMuffins <nopika99@gmail.com>
2022-01-19 13:11:55 -0800

    Rewrote loop in central equation's get_bands() Changed based on hand calculations showing a useful and more readable pattern. New loop produces results that are qualitatively on the right track, at least.

--- central_equation.py
+++ central_equation.py
@@ -43,21 +43,26 @@ def get_bands(path, fourier_coefficients: list, band_count: int = 9) -> list:
     for i in range(band_count):
         energy_bands.append(np.zeros(len(path)))
 
-    a = _inv_index(band_count - 1)[0]
+    # TODO: decide how to handle this coefficient
+    l = 1
 
     energy_idx = 0
     for k in path:
         matrix = np.zeros((band_count, band_count))
         for row in range(len(matrix)): 
-            (x, y) = _inv_index(row)
-            for (gx, gy) in product(range(-a, a+1), repeat = 2):
-                G = x + gx, y + gy
+            for column in range(len(matrix[0])):
+                # with vector indices, the (a,b)th entry in the matrix is U[a-b], remembering that a and b are vectors
+                # when a = b, there is an additional h^2/2m * (k + b)^2 term added in
+                a = np.array(_inv_index(row))
+                b = np.array(_inv_index(column))
                 try:
-                    matrix[row][_index(gx,  gy)] += fourier_coefficients[_index(*G)]
-                except IndexError: # if either index is out of range, we're throwing away that term
-                    pass
-        for i in range(len(matrix)):
-            matrix[i][i] += np.dot(k, k)
+                    matrix[row, column] = fourier_coefficients[_index(*(b - a))]
+                except IndexError:
+                    pass # an index error means the fourier coefficient is outside the defined set, so we assume it is 0
+
+                if all(a == b):
+                    matrix[row, column] += l * np.dot(k + b, k + b)
+
         energies = np.sort(np.linalg.eigvals(matrix))
         for i in range(len(energies)):
             energy_bands[i][energy_idx] = energies[i]
