0e8e53affb95bff94d5e592b3bf98ece57668d21
TheLordOFALlMuffins <nopika99@gmail.com>
2022-02-01 14:21:51 -0800

    Add inverse index functions

--- index.py
+++ index.py
@@ -2,6 +2,9 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
+from math import isqrt
+
+from itertools import count, product
 
 
 def _index2(t: Sequence[int, int]) -> int:
@@ -30,6 +33,54 @@ def index(t: Sequence[int]) -> int:
             return _index3(t)
         case _:
             raise ValueError(f"index not implemented for inputs of length {len(t)}")
+
+
+def _inv_index2(n: int) -> tuple[int, int]:
+    s = isqrt(n)
+    if a := n - s*s < s:
+        b = s
+    else:
+        a, b = s, n - s*s - s
+    return a, b
+
+def _icbrt(n: int) -> int:
+    r = 0
+    for i in count(1):
+        if i**3 > n:
+            return r
+        else:
+            r = i
+
+def _euclidean_divide(quotient: int, dividend: int) -> tuple[int, int]:
+    return quotient//dividend, quotient%dividend
+
+def _inv_index3(n: int) -> int:
+    c = _icbrt(n)
+    if (m := n - c**3 - 2*c*c - c) >= 0:
+        x, y = _euclidean_divide(m, c+1)
+        return c, x, y
+    else:
+        m = n - c**3
+        if m % (2*c+1) >= c:
+            x, y = _euclidean_divide(m, 2*c+1)
+            y = y - c
+            return x, c, y
+        else:
+            x, y = _euclidean_divide(m, 2*c+1)
+            return x, y, c
+
+def inv_index(n: int, dim: int) -> tuple[int, ...]:
+    match dim:
+        case 1:
+            return n
+        case 2:
+            return _inv_index2(n)
+        case 3:
+            return _inv_index3(n)
+        case a if a < 1:
+            raise ValueError("Dimensions less than 1 are invalid")
+        case _:
+            raise ValueError("Indices not implemented for dim > 3")
 
 
 def int_to_n(i: int) -> int:
