9095bf3dc9a5339705bd02b3450979e498f65fa0
TheLordOFALlMuffins <nopika99@gmail.com>
2022-02-01 14:47:07 -0800

    Move indexing functions from central_equaiton.py Replaced with imports fom indexing.py, tweaking the code to work with the slightly different input semantics

--- central_equation.py
+++ central_equation.py
@@ -2,41 +2,16 @@
 from __future__ import annotations
 
 from itertools import product, tee
-from math import isqrt
 from numbers import Number
 
 import numpy as np
 import matplotlib.pyplot as p
 
 from json_interface import load_lattice
+from indexing import int_index as index
+from indexing import inv_int_index as inv_index
 
 np.set_printoptions(linewidth = 100000)
-
-def _index(x: int, y: int) -> int:
-    "Maps 2 integers into a unique natural number, or 0"
-    # maps all integers onto the nonnegative integers
-    a = 2*x if x >= 0 else -2*x-1
-    b = 2*y if y >= 0 else -2*y-1
-    return a*a + a + b if a >= b else a + b*b
-
-def _inv_index(n: int) -> tuple[int, int]:
-    s = isqrt(n)
-    if n - s*s < s:
-        # we landed in the else case above, s = b
-        a, b = n - s*s, s
-    else:
-        # in the if, s = a
-        a, b = s, n - s*s - s
-    # now we undo the mapping of integers onto nonnegative integers
-    if a % 2 == 0:
-        a = a//2
-    else:
-        a = -(a+1)//2
-    if b % 2 == 0:
-        b = b//2
-    else:
-        b = -(b+1)//2
-    return a, b
 
 
 def get_energies(k, band_count, fourier_coefficients: list):
@@ -48,10 +23,10 @@ def get_energies(k, band_count, fourier_coefficients: list):
         for column in range(len(matrix[0])):
             # with vector indices, the (a,b)th entry in the matrix is U[a-b], remembering that a and b are vectors
             # when a = b, there is an additional h^2/2m * (k + b)^2 term added in
-            a = np.array(_inv_index(row))
-            b = np.array(_inv_index(column))
+            a = np.array(inv_index(row, len(k)))
+            b = np.array(inv_index(column, len(k)))
             try:
-                matrix[row, column] = fourier_coefficients[_index(*(a - b))]
+                matrix[row, column] = fourier_coefficients[index(a - b)]
             except IndexError:
                 pass # an index error means the fourier coefficient is outside the defined set, so we assume it is 0
 
@@ -79,11 +54,12 @@ def get_bands(path, fourier_coefficients: list, band_count: int = 9) -> list:
 if __name__ == "__main__":
     # temp code to test a "more physical" potential
     class typical_v(list):
-        def __init__(self, scale: Number = 1):
+        def __init__(self, scale: Number = 1, dimension = 2):
             super().__init__()
             self.scale = scale
+            self.dimension = 2
         def __getitem__(self, idx):
-            x, y = _inv_index(idx)
+            x, y = inv_index(idx, self.dimension)
             if x == y == 0:
                 return 0
             else:
