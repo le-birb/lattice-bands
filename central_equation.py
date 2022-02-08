5b2a24dd1807ed50db58ecba4beb5b01bca21491
TheLordOFALlMuffins <nopika99@gmail.com>
2022-02-08 10:18:51 -0800

    Move potentials into their own file Also changes potentials into functions to make them easier to implement

--- central_equation.py
+++ central_equation.py
@@ -1,8 +1,10 @@
 
 from __future__ import annotations
+from functools import partial
 
 from itertools import product, tee
 from numbers import Number
+from typing import Callable
 
 import numpy as np
 import matplotlib.pyplot as p
@@ -10,11 +12,12 @@ import matplotlib.pyplot as p
 from json_interface import load_lattice
 from indexing import int_index as index
 from indexing import inv_int_index as inv_index
+from potentials import *
 
 np.set_printoptions(linewidth = 100000)
 
 
-def get_energies(k, band_count, fourier_coefficients: list):
+def get_energies(k, band_count, fourier_coefficients: Callable):
     # temporarily set to 1, here to change later if it matters
     l = 1
 
@@ -26,7 +29,7 @@ def get_energies(k, band_count, fourier_coefficients: list):
             a = np.array(inv_index(row, len(k)))
             b = np.array(inv_index(column, len(k)))
             try:
-                matrix[row, column] = fourier_coefficients[index(a - b)]
+                matrix[row, column] = fourier_coefficients(a - b)
             except IndexError:
                 pass # an index error means the fourier coefficient is outside the defined set, so we assume it is 0
 
@@ -35,7 +38,7 @@ def get_energies(k, band_count, fourier_coefficients: list):
     return np.linalg.eigvals(matrix)
 
 
-def get_bands(path, fourier_coefficients: list, band_count: int = 9) -> list:
+def get_bands(path, fourier_coefficients: Callable, band_count: int = 9) -> list:
     energy_bands = []
     for i in range(band_count):
         energy_bands.append(np.zeros(len(path)))
@@ -52,21 +55,6 @@ def get_bands(path, fourier_coefficients: list, band_count: int = 9) -> list:
 
 
 if __name__ == "__main__":
-    # temp code to test a "more physical" potential
-    class typical_v(list):
-        def __init__(self, scale: Number = 1, dimension = 2):
-            super().__init__()
-            self.scale = scale
-            self.dimension = dimension
-
-        def __getitem__(self, idx):
-            x, y = inv_index(idx, self.dimension)
-            if x == y == 0:
-                return 0
-            else:
-                return self.scale/(x**2 + y**2)
-
-
     def pairwise(iterable):
         "s -> (s0,s1), (s1,s2), (s2, s3), ..."
         a, b = tee(iterable)
@@ -91,15 +79,13 @@ if __name__ == "__main__":
     for point in lat.vertical_lines:
         p.axvline(point, linestyle = "--", color = (.5, .5, .5, .5))
 
-    v_simple = [0, 0, 0, 1, 0, 1, 0, 1, 1]
-    vt = typical_v(10)
-    zero_v = []
+    vt = partial(typical_v, scale = 10)
 
     band_count = 9
 
     for i, path in enumerate(paths):
         bands = get_bands(path, vt, band_count)
-        bands_0 = get_bands(path, zero_v, band_count)
+        bands_0 = get_bands(path, empty_v, band_count)
         for band_0, band in zip(bands_0, bands):
             ax.plot(plot_ranges[i], band_0, color = (.5, .5, .5), linestyle = "dashed")
             ax.plot(plot_ranges[i], band, "-k")
---
+++ potentials.py
@@ -0,0 +1,21 @@
+
+
+from typing import Sequence
+
+
+def empty_v(g: Sequence[int]):
+    """The potential that is 0 everywhere"""
+    return 0
+
+def simple_v(g: Sequence[int], scale: float = 1):
+    """A very simple potential where 
+    U(1,0) == U(0,1) == U(-1,0) == U(0,-1) == 1, 
+    and U == 0 otherwise."""
+    return sum(map(lambda x: abs(x) == 1, g)) == 1
+
+def typical_v(g: Sequence[int], scale: float= 1):
+    """A lattice potential that is in some sense "typical":
+    it has U ~ 1/G**2"""
+    if all(gn == 0 for gn in g):
+        return 0
+    return scale/sum(gn**2 for gn in g)
