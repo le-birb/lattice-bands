fe2e89373c3feaf6daa2606c48d972acea94933d
TheLordOfAllMuffins <nopika99@gmail.com>
2022-07-16 17:05:14 -0700

    Clean up directory and old code

--- central_equation.py
+++ central_equation.py
@@ -23,28 +23,13 @@ def get_energies(k, band_count, fourier_coefficients: Callable):
 
     matrix = np.zeros((band_count, band_count))
 
-    # before calculaating matrix entries, figure out a bound below which matrix terms should be thrown out
-    cutoff = 0
-
-    # row_ends = zip(range(matrix.shape[0]), repeat(matrix.shape[1]))
-    # col_ends = zip(repeat(matrix.shape[0]), range(matrix.shape[1]))
-    # for row, column in chain(row_ends, col_ends):
-    #     a = np.array(inv_index(row, len(k)))
-    #     b = np.array(inv_index(column, len(k)))
-    #     u = fourier_coefficients(a - b)
-    #     print(a, b, u)
-    #     if u > cutoff:
-    #         cutoff = u
-
-
     for row, column in np.ndindex(matrix.shape):
         # with vector indices, the (a,b)th entry in the matrix is U[a-b], remembering that a and b are vectors
         # when a = b, there is an additional h^2/2m * (k + b)^2 term added in
         a = np.array(inv_index(row, len(k)))
         b = np.array(inv_index(column, len(k)))
         u = fourier_coefficients(a - b)
-        if u >= cutoff:
-            matrix[row, column] = u
+        matrix[row, column] = u
 
         if all(a == b):
             matrix[row, column] += l * np.dot(k + b, k + b)
@@ -66,41 +51,3 @@ def get_bands(path, fourier_coefficients: Callable, band_count: int = 9) -> list
 
     return energy_bands
 
-
-if __name__ == "__main__":
-    def pairwise(iterable):
-        "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-        a, b = tee(iterable)
-        next(b, None)
-        return zip(a, b)
-
-    lat = load_lattice("lattices/2d_square.json")
-
-    resolution = 50
-
-    paths = []
-    plot_ranges = []
-    curr_range_start = 0
-
-    for start, end in pairwise(lat.points):
-        paths.append(np.linspace(start, end, num = resolution))
-        plot_ranges.append(np.linspace(curr_range_start, curr_range_start + 1, num = resolution))
-        curr_range_start += 1
-
-    fig = p.figure()
-    ax = fig.add_subplot(1,1,1)
-    for point in lat.vertical_lines:
-        p.axvline(point, linestyle = "--", color = (.5, .5, .5, .5))
-
-    vt = partial(typical_v, scale = 1)
-
-    band_count = 9
-
-    for i, path in enumerate(paths):
-        bands = get_bands(path, vt, band_count)
-        bands_0 = get_bands(path, empty_v, band_count)
-        for band_0, band in zip(bands_0, bands):
-            ax.plot(plot_ranges[i], band_0, color = (.5, .5, .5), linestyle = "dashed")
-            ax.plot(plot_ranges[i], band, "-k")
-
-    p.show()
